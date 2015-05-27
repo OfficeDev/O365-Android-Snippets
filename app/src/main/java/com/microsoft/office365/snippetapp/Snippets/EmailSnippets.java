b128d08b9dd0cb46c42af44f236d6e14ea5bb382
Brian Melton <brianmel@microsoft.com>
2015-05-26 18:16:01 -0700

    Formatting

--- app/src/main/java/com/microsoft/office365/snippetapp/Snippets/EmailSnippets.java
+++ app/src/main/java/com/microsoft/office365/snippetapp/Snippets/EmailSnippets.java
@@ -269,9 +269,9 @@ public class EmailSnippets {
      * @version 1.0
      */
     public String addDraftMail(
-            final String emailAddress
-            , final String subject
-            , final String body) throws ExecutionException, InterruptedException {
+            final String emailAddress,
+            final String subject,
+            final String body) throws ExecutionException, InterruptedException {
         // Prepare the message.
         List<Recipient> recipientList = new ArrayList<>();
 
